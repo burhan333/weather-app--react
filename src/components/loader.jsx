9daa8b2fa1b3659098d258b0d18fe057b85bd4b5
burhan333 <em.burhan@yahoo.com>
2021-12-26 03:30:11 +0500

    loader

---
+++ src/components/loader.jsx
@@ -0,0 +1,9 @@
+const Loader = () => {
+    return(
+        <div className="loader">
+            <div class="lds-grid"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
+        </div>
+    )
+}
+
+export default Loader
